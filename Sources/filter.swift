02850715b75e6cf27b0994687c7988d4ccd9b899
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2022-03-04 15:02:18 +0100

    Added `Sequence.asyncFilter(_:)` and a throwing and non-throwing version of `Sequence.concurrentFilter(withPriority:operation:)`.

---
+++ Sources/filter.swift
@@ -0,0 +1,47 @@
+//
+//  filter.swift
+//  
+//
+//  Created by Jeremy Bannister on 2/24/22.
+//
+
+// MARK: - Filter
+
+@available(iOS 13.0, macOS 10.15.0, watchOS 6, tvOS 13, *)
+public extension Sequence {
+    
+    ///
+    func asyncFilter
+        (_ operation: (Element)async throws->Bool)
+    async rethrows -> Array<Element> {
+        
+        ///
+        try await self.asyncCompactMap {
+            try await operation($0) ? $0 : nil
+        }
+    }
+
+    ///
+    func concurrentFilter
+        (withPriority priority: TaskPriority? = nil,
+         _ operation: @escaping (Element)async->Bool)
+    async -> Array<Element> {
+        
+        ///
+        await self.concurrentCompactMap(withPriority: priority) {
+            await operation($0) ? $0 : nil
+        }
+    }
+
+    ///
+    func concurrentFilter
+        (withPriority priority: TaskPriority? = nil,
+         _ operation: @escaping (Element)async throws->Bool)
+    async throws -> Array<Element> {
+        
+        ///
+        try await self.concurrentCompactMap(withPriority: priority) {
+            try await operation($0) ? $0 : nil
+        }
+    }
+}
